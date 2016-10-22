91d3c2ec5982ed8f1ddd002bff240aa90f2d545c
Carl Hofmeister <hofmeister.carl@gmail.com>
2016-10-22 15:13:06 -0600

    Added arduino sketch for rockets.

---
+++ rocket-sensor/rocket-sensor.ino
@@ -0,0 +1,24 @@
+#include "bmp180.h"
+#include <avr/eeprom.h>
+#define SENSORS_PRESSURE_SEALEVELHPA (1013.25F)
+
+float max_alt = 0;
+
+
+
+void setup() {
+    bmpinit(BMP085_MODE_ULTRAHIGHRES);
+}
+
+void loop() {
+    float pressure = getPressure();
+    float seaLevelPressure = SENSORS_PRESSURE_SEALEVELHPA;
+    float altitude = pressureToAltitude(seaLevelPressure, pressure/100);
+    if(altitude > max_alt) {
+        max_alt = altitude;
+        eeprom_write_float(0, max_alt);
+    }
+
+    delay(100);
+
+}

fb1de378a939aebaffc6ce8aca09839340463525
Carl Hofmeister <hofmeister.carl@gmail.com>
2016-12-28 16:50:49 -0600

    Added some documentation to the VESC library.

--- VESCArduinoExample/VESCArduinoExample.ino
+++ VESCArduinoExample/VESCArduinoExample.ino
@@ -1,4 +1,4 @@
-#include <VESCPacket.h>
+#include "VESCPacket.h"
 byte* inByte;
 byte id = 100;
 char* outByte;
@@ -12,7 +12,7 @@ void setup() {
   inByte = (byte*)malloc(sizeof(byte) * 256);
   outByte = (char*)malloc(sizeof(char) * 256);
   payload = (uint8_t*)malloc(sizeof(uint8_t) * 256);
-  Serial.begin(9600);
+  Serial.begin(115200);
   while(!Serial);
 }
 
@@ -20,27 +20,23 @@ void loop() {
   // put your main code here, to run repeatedly:
   if(Serial.available()>0){
       ReadVESCMessage(inByte);
-      UnpackPayload(inByte, sizeof(inByte)/sizeof(byte), payload, &lenPay);
-      if(payload[0] == 36){
-        char msg[] = {'t','e','s','t'};
+      UnpackMessage(inByte, sizeof(inByte)/sizeof(byte), payload, &lenPay);
+      if(payload[0] == 36){ // subscription request
+        char msg[] = {'t','e','s','t'}; 
         //memcpy(payload, msg, length(msg));
         
         SendVESCPacket((uint8_t*)msg, sizeof(msg)/sizeof(char));
-        }
+      }
       if(payload[0] == 1){
         if(payload[1] == 1){
           digitalWrite(13,HIGH);
-          }
-        else if(payload[1] == 0){
+        } else if(payload[1] == 0){
           digitalWrite(13, LOW);
-          }
         }
-    }
-    else if(cnt++ > 2000){
+      }
+   } else if(cnt++ > 2000){
       char msg[] = {'H','E','L','L','O'};
       SendVESCPacket((uint8_t*)msg, sizeof(msg)/sizeof(char));
       cnt = 0;
-      }
-   
-      
+   }
 }

8c6e1a1670c61be6854ea873569de87f5173e98f
Carl Hofmeister <hofmeister.carl@gmail.com>
2017-07-01 15:08:33 -0600

    Updated Lidar program to use new mounting system.

--- LIDAR/LIDAR.ino
+++ LIDAR/LIDAR.ino
@@ -3,15 +3,15 @@
 /* Version: 1.0.0
 /* By: scharette
 /* Date: 2015-03-31
-/* 
+/*
 /* The LIDAR lite is attached to VCC (+5 V DC), GND, SDA and SCL.
- *  
+ *
  */
-/* 
+/*
 /* This examples uses the Wire library.
 /*
 /* Hardware used:
- * 
+ *
 /* BotBoarduino: http://www.robotshop.com/en/lynxmotion-botboarduino-robot-controller.html?product-code=RB-Lyn-363
 /* LIDAR-Lite Laser Rangefinder: http://www.robotshop.com/en/lidar-lite-laser-rangefinder-pulsedlight.html?product-code=RB-Pli-01
 /* HS-645MG servo motor: http://www.robotshop.com/en/hitec-hs645mg-servo-motor.html?product-code=RB-Hit-29
@@ -19,8 +19,6 @@
 /* Bracket to hold the LIDAR: http://www.robotshop.com/en/aluminum-l-connector-bracket-asb-06.html?product-code=RB-Lyn-83
 /* or this one: http://www.robotshop.com/en/lynxmotion-multi-purpose-sensor-housing.html?product-code=RB-Lyn-75
 /* Bracket to hold the servo motor: http://www.robotshop.com/en/lynxmotion-aluminum-multi-purpose-servo.html?product-code=RB-Lyn-81
- *  
- * Stepper motor library: https://github.com/laurb9/StepperDriver  
  *
 /*
 /* *: Please note that while we used a HS-645MG in our example, the HS-422 and almost any other standard RC servo motor could perform this task.
@@ -28,30 +26,29 @@
 /**********************************************************************************************************/
 
 #include <Wire.h>
-#include "A4988.h"
+#include "A4988.h" // Stepper motor library: https://github.com/laurb9/StepperDriver
 #include "VESCPacket.h"
+#include <Servo.h>
 
 #define    LIDARLite_ADDRESS   0x62          // Default I2C Address of LIDAR-Lite.
 #define    RegisterMeasure     0x00          // Register to write to initiate ranging.
 #define    MeasureValue        0x04          // Value to initiate ranging.
 #define    RegisterHighLowB    0x8f          // Register to get both High and Low bytes in 1 call.
 
-#define MOTOR_STEPS 200
+#define MOTOR_STEPS 600 // 3:1 belt ratio
 #define DIR 8
 #define STEP 9
-#define MS1 10
- #define MS2 11
- #define MS3 12
- A4988 stepper(MOTOR_STEPS, DIR, STEP, MS1, MS2, MS3);
+#define MOTOR_DELAY 1 // 1/2 period of pulse to the stepper
 
-#define MICROSTEPS 16
 
-#include <Servo.h>
-
-Servo myservo;
+Servo LidarPitch;
 
 double pos = 0;         // Position of the servo (degress, [0, 180])
 int distance = 0;    // Distance measured
+int lidar_pitch = 45;
+int pitch_step = 3;
+#define PITCH_UPPER 50 // Don't go higher than 60, mount might break
+#define PITCH_LOWER 35
 
 int second = 0;
 int count = 0;
@@ -67,20 +64,22 @@ void setup()
   subscribe(BLINK_LED, blink_led);
   // Serial output
   Serial.begin(115200);
-  
+  pinMode(DIR, OUTPUT);
+  pinMode(STEP, OUTPUT);
+  digitalWrite(DIR, LOW);
+
   // Servo control
-  myservo.attach(5);
-  
+  LidarPitch.attach(5);
+  LidarPitch.write(lidar_pitch);
+
   // LIDAR control
   Wire.begin(); // join i2c bus
   //setAcquisition(200);
-  stepper.setRPM(10);
-  stepper.setMicrostep(16); 
 }
 void setAcquisition(int val){
   Wire.beginTransmission((int)LIDARLite_ADDRESS); // transmit to LIDAR-Lite
-  Wire.write(0x02); // sets register pointer to  (0x00)  
-  Wire.write(val); // sets register pointer to  (0x00)  
+  Wire.write(0x02); // sets register pointer to  (0x00)
+  Wire.write(val); // sets register pointer to  (0x00)
   Wire.endTransmission(); // stop transmitting
   }
 
@@ -88,20 +87,20 @@ void setAcquisition(int val){
 int lidarGetRange(void)
 {
   int val = -1;
-  
+
   Wire.beginTransmission((int)LIDARLite_ADDRESS); // transmit to LIDAR-Lite
-  Wire.write((int)RegisterMeasure); // sets register pointer to  (0x00)  
-  Wire.write((int)MeasureValue); // sets register pointer to  (0x00)  
+  Wire.write((int)RegisterMeasure); // sets register pointer to  (0x00)
+  Wire.write((int)MeasureValue); // sets register pointer to  (0x00)
   Wire.endTransmission(); // stop transmitting
 
-  delay(10); // Wait 20ms for transmit
+  delay(15); // Wait 20ms for transmit
 
   Wire.beginTransmission((int)LIDARLite_ADDRESS); // transmit to LIDAR-Lite
   Wire.write((int)RegisterHighLowB); // sets register pointer to (0x8f)
   Wire.endTransmission(); // stop transmitting
 
-  delay(10); // Wait 20ms for transmit
-  
+  delay(15); // Wait 20ms for transmit
+
   Wire.requestFrom((int)LIDARLite_ADDRESS, 2); // request 2 bytes from LIDAR-Lite
 
   if(2 <= Wire.available()) // if two bytes were received
@@ -110,7 +109,7 @@ int lidarGetRange(void)
     val = val << 8; // shift high byte to be high 8 bits
     val |= Wire.read(); // receive low byte as lower 8 bits
   }
-  
+
   return val;
 }
 
@@ -127,7 +126,7 @@ void serialPrintRange(double pos, int distance)
 {
     Serial.print("position (deg): ");
     Serial.print(pos);
-    
+
     Serial.print("\t\tDistance (cm): ");
     Serial.println(distance);
 }
@@ -142,17 +141,45 @@ void timeMeasure(){
         count = count + 1;
       }
     serialPrintRange(second,count, distance);
-    
+
   }
+
+void update_pitch() {
+  LidarPitch.write(lidar_pitch);
+  if (pitch_step > 0) {
+      if (lidar_pitch <= PITCH_UPPER) {
+        lidar_pitch += pitch_step;
+      } else {
+        pitch_step = -pitch_step;
+      }
+  } else {
+      if (lidar_pitch >= PITCH_LOWER) {
+        lidar_pitch += pitch_step;
+      } else {
+        pitch_step = -pitch_step;
+      }
+  }
+}
+
+void step_motor() {
+    digitalWrite(STEP, HIGH);
+    delay(MOTOR_DELAY);
+    digitalWrite(STEP, LOW);
+    delay(MOTOR_DELAY);
+}
+
 void loop()
 {
-  for(int i = 0; i < 200; i++){
+  update_pitch();
+  for(int i = 0; i < MOTOR_STEPS; i++){
     distance = lidarGetRange();
-    //serialPrintRange(pos, distance);
+    char buff[256];
+    /* sprintf(buff, "distance: %f\t angle: %f, pitch: %f", distance/100.0, pos, lidar_pitch-45); */
+    /* Serial.println(buff); */
     LidarDataMessage msg = LidarDataMessage(distance, pos);
     SendVESCPacket(&msg);
-    stepper.move(1);
-    pos = pos + 1.8; 
-    }
-    pos = 0;
+    pos = pos + 0.6;
+    step_motor();
+  }
+  pos = 0;
 }

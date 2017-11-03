dc687fe89ad310b872b9ca9c1005432a3c93c769
Carl Hofmeister <hofmeister.carl@gmail.com>
2017-11-03 17:42:08 -0600

    Got d21-rkt-board altitude logging fully functional

--- rocket-sensor/d21-rkt-board/d21-rkt-board.ino
+++ rocket-sensor/d21-rkt-board/d21-rkt-board.ino
@@ -19,6 +19,7 @@
 #include <SPI.h>
 #include <Adafruit_Sensor.h>
 #include <Adafruit_BMP280.h>
+#include <FlashStorage.h>
 
 #define BMP_SCK 13
 #define BMP_MISO 12
@@ -29,30 +30,42 @@ Adafruit_BMP280 bmp; // I2C
 //Adafruit_BMP280 bmp(BMP_CS); // hardware SPI
 //Adafruit_BMP280 bmp(BMP_CS, BMP_MOSI, BMP_MISO,  BMP_SCK);
 
+double max_alt;
+FlashStorage(max_alt_storage, double);
+
+void check_altitude() {
+  double current_alt = bmp.readAltitude(1013.25);
+  SerialUSB.print("Current alt: ");
+  SerialUSB.println(current_alt);
+  if(current_alt > max_alt) {
+    max_alt = current_alt;
+    max_alt_storage.write(max_alt);
+  }
+}
+
+void reset_max_alt() {
+  max_alt_storage.write(0.0);
+}
+
 void setup() {
  SerialUSB.begin(9600);
- while (!SerialUSB);
+ //while (!SerialUSB);
  SerialUSB.println(F("BMP280 test"));
  
- if (!bmp.begin(0x76,0x58)) {  
+ if (!bmp.begin(0x76/*,0x58 */)) {  
    SerialUSB.println(F("Could not find a valid BMP280 sensor, check wiring!"));
    while (1);
  }
+ max_alt = max_alt_storage.read();
+ delay(2000);// First reading is completely wrong without this
 }
 
 void loop() {
-   SerialUSB.print(F("Temperature = "));
-   SerialUSB.print(bmp.readTemperature());
-   SerialUSB.println(" *C");
-   
-   SerialUSB.print(F("Pressure = "));
-   SerialUSB.print(bmp.readPressure());
-   SerialUSB.println(" Pa");
 
-   SerialUSB.print(F("Approx altitude = "));
-   SerialUSB.print(bmp.readAltitude(1015.92)); // this should be adjusted to your local forcase
+   SerialUSB.print(F("Approx max altitude = "));
+   SerialUSB.print(max_alt);
    SerialUSB.println(" m");
-   
+   check_altitude();
    SerialUSB.println();
-   delay(2000);
+   delay(500);
 }

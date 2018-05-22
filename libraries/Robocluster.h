795a0341b6a156cbae193ccdc3e9053a5b871f3a
Carl Hofmeister <hofmeister.carl@gmail.com>
2018-05-21 19:17:42 -0600

    Added callbacks to robocluster.h

--- libraries/Robocluster.h
+++ libraries/Robocluster.h
@@ -1,14 +1,27 @@
 
+void dummy_handler(char*) {
+    // This does nothing, but you can define your own
+    // function and enable it with set_message_handler(yourFunction).
+}
+
 char *DEVICE_NAME = "TestName";
+void (*g_message_handler)(char*) = dummy_handler;
+
 
 void serialEvent() {
     digitalWrite(13, HIGH);
     char buff[1024];
-    sprintf(buff, "{\"name\":\"%s\"}", DEVICE_NAME);
     char b[10];
-    Serial.readBytes(b, 7);
-    Serial.write(strlen(buff));
-    Serial.print(buff);
+    b[0] = Serial.read();
+    if (b[0] == 0x02 || b[0] == 0x03) {
+        Serial.readBytes(b, 6);
+        sprintf(buff, "{\"name\":\"%s\"}", DEVICE_NAME);
+        Serial.write(strlen(buff));
+        Serial.print(buff);
+    } else {
+        Serial.readBytes(buff, b[0]);
+        g_message_handler(buff);
+    }
     digitalWrite(13, LOW);
 }
 
@@ -16,8 +29,14 @@ void set_name(char* s) {
     DEVICE_NAME = s;
 }
 
+void set_message_handler(void (*func)(char*)) {
+    g_message_handler = func;
+}
+
 void Publish(char *message) {
     int len = strlen(message);
     Serial.write(len);
     Serial.print(message);
 }
+
+

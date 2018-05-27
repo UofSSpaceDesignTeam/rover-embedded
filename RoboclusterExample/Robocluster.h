6ed65ed83ba6bedf1fbd525c63911b87a7ea5379
Carl Hofmeister <hofmeister.carl@gmail.com>
2018-05-27 04:43:21 -0600

    Fixed reading json serial messages

--- RoboclusterExample/Robocluster.h
+++ RoboclusterExample/Robocluster.h
@@ -1,19 +1,41 @@
+#ifndef ROBOCLUSTER
+#define ROBOCLUSTER
+
+#define BUFF_SIZE 512
+
+void dummy_handler(char*) {
+    // This does nothing, but you can define your own
+    // function and enable it with set_message_handler(yourFunction).
+}
 
 char *DEVICE_NAME = "TestName";
+void (*g_message_handler)(char*) = dummy_handler;
+
 
 void serialEvent() {
-    digitalWrite(13, HIGH);
-    char buff[1024];
-    sprintf(buff, "{\"name\":\"%s\"}", DEVICE_NAME);
+    //digitalWrite(13, HIGH);
+    char buff[BUFF_SIZE];
+    memset(buff, 0, BUFF_SIZE);
     char b[10];
-    Serial.readBytes(b, 7);
-    Serial.write(strlen(buff));
-    Serial.print(buff);
-    digitalWrite(13, LOW);
+    b[0] = Serial.read();
+    if (b[0] == 0x02 || b[0] == 0x03) {
+        Serial.readBytes(b, 6);
+        sprintf(buff, "{\"name\":\"%s\"}", DEVICE_NAME);
+        Serial.write(strlen(buff));
+        Serial.print(buff);
+    } else {
+        Serial.readBytes(buff, b[0]);
+        g_message_handler(buff);
+    }
+    //digitalWrite(13, LOW);
 }
 
 void set_name(char* s) {
     DEVICE_NAME = s;
+}
+
+void set_message_handler(void (*func)(char*)) {
+    g_message_handler = func;
 }
 
 void Publish(char *message) {
@@ -21,3 +43,13 @@ void Publish(char *message) {
     Serial.write(len);
     Serial.print(message);
 }
+
+void s_delay(int value) {
+    if (Serial.available()) {
+        serialEvent();
+    }
+    delay(value);
+
+}
+
+#endif
--- RoboclusterExample/RoboclusterExample.ino
+++ RoboclusterExample/RoboclusterExample.ino
@@ -1,10 +1,19 @@
 #include "Robocluster.h"
 
+void blink(char *json_msg) {
+    char *expected = "{\"test\": 42}";
+    if (strcmp(json_msg, expected) == 0) {
+        digitalWrite(13, HIGH);
+    }
+    Publish(json_msg);
+}
+
 void setup() {
   // put your setup code here, to run once:
   Serial.begin(115200);
   pinMode(13, OUTPUT); //LED will blink when message received
   set_name("ExampleDevice");
+  set_message_handler(blink);
 }
 
 void loop() {
